6a3e6824d512fe3f8a4156d23599caab36636732
Mark Anthony Chua <MarkAnthony.Chua@infor.com>
2015-08-09 21:31:46 +0800

    Initial Commit

---
+++ CalculatorSite.Tests/Controllers/CalculatorControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorSite.Controllers;
+
+namespace CalculatorSite.Tests.Controllers
+{
+    [TestClass]
+    public class CalculatorControllerTest
+    {
+        CalculatorController controller = new CalculatorController();
+
+        [TestMethod]
+        public void Add()
+        {
+            // Arrange
+
+            // Act
+            decimal result = controller.Add(1.1m, 2.2m);
+
+            // Assert
+            Assert.AreEqual(result, 3.3m);
+
+            // Cleanup
+        }
+
+        [TestMethod]
+        public void Subtract()
+        {
+            // Arrange
+
+            // Act
+            decimal result = controller.Subtract(1.1m, 2.2m);
+
+            // Assert
+            Assert.AreEqual(result, -1.1m);
+
+            // Cleanup
+        }
+
+        [TestMethod]
+        public void Multiply()
+        {
+            // Arrange
+
+            // Act
+            decimal result = controller.Multiply(1.1m, 2.2m);
+
+            // Assert
+            Assert.AreEqual(result, 2.42m);
+
+            // Cleanup
+        }
+
+        [TestMethod]
+        public void Divide()
+        {
+            // Arrange
+
+            // Act
+            //decimal result = controller.Divide(2.2m, 1.1m);
+            decimal result = controller.Divide(0m, 1.1m);
+
+            // Assert
+            Assert.AreEqual(result, 0);
+
+            // Cleanup
+        }
+    }
+}
---
+++ CalculatorSite/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatorSite.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Add(decimal num1, decimal num2)
+        {
+            return Json(num1 + num2, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Subtract(decimal num1, decimal num2)
+        {
+            return Json(num1 - num2, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Multiply(decimal num1, decimal num2)
+        {
+            return Json(num1 * num2, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Divide(decimal num1, decimal num2)
+        {
+            return Json(num1 != 0 ? num1 / num2 : 0, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
